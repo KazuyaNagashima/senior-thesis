68b087ccf1813ff79d7819ffedb41d7bc275a7f1
KazuyaNagashima <58168724+KazuyaNagashima@users.noreply.github.com>
2019-11-25 19:49:22 +0900

    Create ngsplot.sh

---
+++ ngsplot.sh
@@ -0,0 +1,29 @@
+####set data########
+filename1=("fibro3min-190515")
+filename2=("fibro-3min-170624")
+ngs_ref=("sscr11")
+#2_homer.sh
+date="19.10.23"
+######################
+
+#inport data in directory
+
+dir="/home/ohganelab/Desktop/nagashima"
+cd ${dir}/comparison/${date}
+#mkdir ${dir}/comparison/${date}/ngsplot/
+cd ngsplot
+
+#cp ${dir}/${filename1}/${filename1}_sorted.bam ${dir}/comparison/${date}/ngsplot
+#cp ${dir}/${filename2}/${filename2}_sorted.bam ${dir}/comparison/${date}/ngsplot
+
+#echo ${filename1}_sorted.bam   -1  "${filename1}">list.txt
+#echo ${filename2}_sorted.bam   -1  "${filename2}">>list.txt
+
+#ngs.plot.r -G ${ngs_ref} -R genebody -C list.txt -O comparison_genebody -L 4000 -FL 200
+#ngs.plot.r -G ${ngs_ref} -R tss -C list.txt -O comparison_tss -L 4000 -FL 200
+#ngs.plot.r -G ${ngs_ref} -R genebody -C list.txt -O k-means -L 4000 -FL 200 -GO km -KNC 10 -NRS 200
+
+#mkdir genebody
+#mv comparison_genebody.avgprof.pdf comparison_genebody.heatmap.pdf comparison_genebody.zip genebody
+#mkdir tss
+#mv comparison_tss.avgprof.pdf comparison_tss.heatmap.pdf comparison_tss.zip tss
